fc8343a29eda2d48fc2245ec08adffc4fe941099
Yogendra K Meshram <125588349+yogendra-2k24@users.noreply.github.com>
2026-07-24 21:54:09 +0530

    feat(upload): add reume upload endpoint

--- app/routers/resume.py
+++ app/routers/resume.py
@@ -1,18 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 
 router = APIRouter(
     prefix="/resume",
     tags=["Resume"]
 )
 
-@router.get("/")
-def get_resume():
+@router.post("/upload")
+def upload_resume(file: UploadFile=File(...)):
     return {
-        "message": "Resume Router Working"
-    }
-
-@router.get("/upload")
-def upload_resume():
-    return {
-        "message": "Resume Uploaded Successfully"
+        "filename": file.filename
     }
