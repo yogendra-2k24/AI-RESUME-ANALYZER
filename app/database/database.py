597bbc88e4cea141aa126e51803d395ba7fed4e7
Yogendra K Meshram <125588349+yogendra-2k24@users.noreply.github.com>
2026-07-29 20:52:22 +0530

    feat(database): configure SQLAlchemy session factory

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = (
     "postgresql://postgres:Yogesh123@localhost:5432/resume_analyzer"
 )
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
