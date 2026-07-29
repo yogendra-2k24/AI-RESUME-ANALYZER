91b0c5dba31a8251ccd81ed6529fa3ef1156ab82
Yogendra K Meshram <125588349+yogendra-2k24@users.noreply.github.com>
2026-07-30 00:39:41 +0530

    feat(models): define ResumeAnalysis ORM model

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, Float, JSON,DateTime
+from datetime import datetime, UTC
+defualt=lambda: datetime.now(UTC)
 from sqlalchemy.orm import Mapped, mappped_column
 
 from app.database.base import Base
@@ -10,4 +12,34 @@ class ResumeAnalysis(Base):
     id: Mapped[int] = mappped_column(
         Integer,
         primary_key=True
+    )
+
+    filename: Mapped[str] = mappped_column(
+        String(255),
+        nullable = False
+    )
+
+    file_path: Mapped[str] = mappped_column(
+        String(500),
+        nullable = False
+    )
+
+    ats_score: Mapped[float] = mappped_column(
+        Float,
+        nullable = False
+    )
+
+    missing_skills: Mapped[list[str]] = mappped_column(
+        JSON,
+        nullable = False
+    )
+
+    suggestions: Mapped[list[str]] = mappped_column(
+        JSON,
+        nullable = False
+    )
+
+    created_at: Mapped[datetime] = mappped_column(
+        DateTime,
+        default=datetime.now
     )
